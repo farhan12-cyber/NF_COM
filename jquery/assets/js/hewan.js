061e46b0acd71c42773d6ba59dc7e3ee51fb5196
Muhammad Farhan <72549949+farhan12-cyber@users.noreply.github.com>
2022-09-25 23:22:08 +0700

    menambahkan file tugas 7

---
+++ jquery/assets/js/hewan.js
@@ -0,0 +1,147 @@
+$(document).ready(function(){
+    //sapi
+    $(".cow").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/sapi.jpg",
+            'class':"img-fluid",
+        }).appendTo(".cow");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/cow.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".cow")
+    })
+    //domba
+    $(".kambing").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/kambing.jpeg",
+            'class':"img-fluid",
+        }).appendTo(".kambing");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/kambing.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".kambing")
+    })
+    //kerbau
+    $(".kebo").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/kebo.jpg",
+            'class':"img-fluid",
+        }).appendTo(".kebo");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/buffalo.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".kebo")
+    })
+    //alpaka
+    $(".babi").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/babi.jpg",
+            'class':"img-fluid",
+        }).appendTo(".babi");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/babi.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".babi")
+    })
+    //alpaka
+    $(".chicken").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/ayam.jpg",
+            'class':"img-fluid",
+        }).appendTo(".chicken");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/chicken.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".chicken")
+    })
+    //merpati
+    $(".tua").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/katua.jpg",
+            'class':"img-fluid",
+        }).appendTo(".tua");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/kk.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".tua")
+    })
+    //bebek
+    $(".duck").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/bebek.jpg",
+            'class':"img-fluid",
+        }).appendTo(".duck");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/duck.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".duck")
+    })
+    //bebek
+    $(".beo").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/beo.jpeg",
+            'class':"img-fluid",
+        }).appendTo(".beo");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/beo.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".beo")
+    })
+    // Hewan Buas
+    //singa
+    $(".lion").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/singa.jpg",
+            'class':"img-fluid",
+        }).appendTo(".lion");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/lion.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".lion")
+    })
+    //harimau
+    $(".tiger").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/harimau.jpeg",
+            'class':"img-fluid",
+        }).appendTo(".tiger");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/tiger.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".tiger")
+    })
+    //hiu
+    $(".wolf").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/serigala.jpg",
+            'class':"img-fluid",
+        }).appendTo(".wolf");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/wolf.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".wolf")
+    })
+    //komodo
+    $(".gajah").click(function(){
+        $("<img>").attr({
+            'src':"assets/image/gajjah.jpg",
+            'class':"img-fluid",
+        }).appendTo(".gajah");
+        $("<audio></audio>").attr({
+            'src':"assets/sound/gajah.mp3",
+            'volume': 0.3,
+            'autoplay': "autoplay"
+        }).appendTo(".gajah")
+    })
+})
